dbf2bac7fed179f25956d8783ab619de31131288
Jason Eslick <jslick@gmail.com>
2012-10-28 16:32:43 -0500

    Set org name and app name

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+const QString APP_ORGNAME = "PepperNote";
+const QString APP_APPNAME = "PepperNote";
+
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName(APP_ORGNAME);
+    QCoreApplication::setApplicationName(APP_APPNAME);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
