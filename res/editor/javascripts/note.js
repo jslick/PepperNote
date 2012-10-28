476c290cbbb0b892c9d00ef3b58b9eb5f324aec7
Jason Eslick <jslick@gmail.com>
2012-10-28 13:31:01 -0500

    Catch clicks outside of editable area
    Clicks outside of editable area are caught and the cursor is placed at
    the end of the editable area.
    
    Unfortunately, this implementation requires that the body be 100% tall,
    which creates an unnecessary scrollbar (depending on the content).

--- res/editor/javascripts/note.js
+++ res/editor/javascripts/note.js
@@ -1,8 +1,31 @@
 var ENABLE_FIREBUG = false;
 
+/**
+ * If any part of the body is clicked outside of the editable area, the cursor
+ * is placed at the end of the #note_content.
+ *
+ * NOTE:  For this to work, body height must be 100%.
+ */
+function acceptAllClicks() {
+    var noteContentClicked = false;
+
+    document.body.observe('click', function(event) {
+        if (!noteContentClicked)
+            putCursorAtEnd($('note_content'));
+        noteContentClicked = false;
+    });
+
+    $('note_content').observe('click', function(event) {
+        noteContentClicked = true;
+    });
+}
+
 document.observe('dom:loaded', function() {
     if (ENABLE_FIREBUG)
         addFirebug();
+
+    // Clicks outside the editable area are caught
+    acceptAllClicks();
 
     $('note_content').focus();
 
